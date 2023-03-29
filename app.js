4cd8fa4c227fb033411c2c908eae594a3a4c4ffd
Yuriy <shade@MacBook-M1Pro.local>
2023-03-29 22:26:26 +0300

    Player test

--- app.js
+++ app.js
@@ -39,3 +39,47 @@ document.addEventListener("DOMContentLoaded", () => {
 //     }
 });
 
+
+let videoSource = new Array();
+videoSource[0] = 'https://dana.auction/intro-video.mp4';
+videoSource[1] = 'https://dana.auction/intro-video.mp4';
+let i = 0; // global
+const videoCount = videoSource.length;
+const element = document.getElementById("videoPlayer");
+
+function videoPlay(videoNum) {
+    element.setAttribute("src", videoSource[videoNum]);
+    element.autoplay = true;
+    element.load();
+}
+document.getElementById('videoPlayer').addEventListener('ended', myHandler, false);
+
+videoPlay(0); // load the first video
+ensureVideoPlays(); // play the video automatically
+
+function myHandler() {
+    i++;
+    if (i == videoCount) {
+        i = 0;
+        videoPlay(i);
+    } else {
+        videoPlay(i);
+    }
+}
+
+function ensureVideoPlays() {
+    const video = document.getElementById('videoPlayer');
+
+    if(!video) return;
+    
+    const promise = video.play();
+    if(promise !== undefined){
+        promise.then(() => {
+            // Autoplay started
+        }).catch(error => {
+            // Autoplay was prevented.
+            video.muted = true;
+            video.play();
+        });
+    }
+}
