6ee6a1dd0835240911a9bceb12be11e038625d4d
Yuriy <klinkov@gmail.com>
2023-03-30 12:28:49 +0300

    final php

--- send.php
+++ send.php
@@ -8,21 +8,6 @@ $email = trim($email); // Удалим пробелы в начале и кон�
 
 $curl = curl_init();
 
-// curl_setopt_array($curl, [
-//   CURLOPT_URL => "https://docs.google.com/forms/d/e/1FAIpQLSerI5z96cOF0-N8-tki498-NkheoiyDamPt-Gs813aLa-dFNQ/formResponse",
-//   CURLOPT_RETURNTRANSFER => true,
-//   CURLOPT_ENCODING => "",
-//   CURLOPT_MAXREDIRS => 10,
-//   CURLOPT_TIMEOUT => 30,
-//   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-//   CURLOPT_CUSTOMREQUEST => "POST",
-//   CURLOPT_POSTFIELDS => "entry.175484913=" + trim($_POST["mail"]) + "&entry.150908741=" + $_SERVER['SERVER_NAME'] + "&fbzx=4985693244102912634&partialResponse=%5Bnull%2Cnull%2C%224985693244102912634%22%5D",
-//   CURLOPT_COOKIE => "S=spreadsheet_forms%3DTPOgEOT7-kz5vysns939L7GbH2tyiYwskAF24zM2hpo; COMPASS=spreadsheet_forms%3DCjIACWuJVzailvltnDewjNjsoVIZb7xceiCmiCMf-b6hcn30OHqnlfrsyNJVUnvFNrEsDhCUtpWhBho0AAlriVdkhpdoaR9KpQ7Eag2VQAKWjb8_dWpFhslYXyPc_BdPNveY0wnescN1h0lY4bPWYA%3D%3D; NID=511%3DvD5dC6ceGIQvhNnUBpyhNJfl8GFupoWLTP6SDNovupZr2RrTo-rENaob1MLdam3vEOjHwev4fYroZzn9PO0RKWeCSJr7bVRxPK5O73-uZgTAO_ODiU05cAjMw2_PxCyj8TUXjOiUGOqzd4YxVYYvvM50dcA38RGHZ8MY-0CC-1A",
-//   CURLOPT_HTTPHEADER => [
-//     "Content-Type: application/x-www-form-urlencoded"
-//   ],
-// ]);
-
 curl_setopt_array($curl, [
   CURLOPT_URL => "https://docs.google.com/forms/d/e/1FAIpQLSerI5z96cOF0-N8-tki498-NkheoiyDamPt-Gs813aLa-dFNQ/formResponse",
   CURLOPT_RETURNTRANSFER => true,
@@ -43,10 +28,8 @@ $err = curl_error($curl);
 
 curl_close($curl);
 
-echo ("entry.175484913=" . $email . "&entry.150908741=" . $_SERVER['SERVER_NAME'] . "&fbzx=4985693244102912634&partialResponse=%5Bnull%2Cnull%2C%224985693244102912634%22%5D");
-
-// if ($err) {
-//     header('Location: /index.html?sacerr' + err,true, 302);
-// } else {
-//     header('Location: /index.html?sacses',true, 302);
-// }
+if ($err) {
+  header('Location: /index.html?sacerr' . urlencode($err), true, 302);
+} else {
+  header('Location: /index.html?sacses', true, 302);
+}
