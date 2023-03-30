392f4217c9ce4705ad888a587d2807a451e4f772
Yuriy <klinkov@gmail.com>
2023-03-30 12:11:48 +0300

    php

--- send.php
+++ send.php
@@ -1,6 +1,6 @@
 <?php
 
-$email = $_POST['email']; // Принимаем данные
+$email = $_POST['mail']; // Принимаем данные
 $email = htmlspecialchars($email); // Преобразуем все символы которые пользователь пытается добавить в форму
 $email = urldecode($email); // Декодируем url, если пользователь пытается его добавить в форму
 $email = trim($email); // Удалим пробелы в начале и конце если такие имеются
@@ -16,7 +16,7 @@ curl_setopt_array($curl, [
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => "entry.175484913="+$_POST["email"]+"&entry.150908741="+$_REQUEST["HTTP_REFERER"]+"&fbzx=4985693244102912634&partialResponse=%5Bnull%2Cnull%2C%224985693244102912634%22%5D",
+  CURLOPT_POSTFIELDS => "entry.175484913=" + trim($_POST["mail"]) + "&entry.150908741=" + $_SERVER['SERVER_NAME'] + "&fbzx=4985693244102912634&partialResponse=%5Bnull%2Cnull%2C%224985693244102912634%22%5D",
   CURLOPT_COOKIE => "S=spreadsheet_forms%3DTPOgEOT7-kz5vysns939L7GbH2tyiYwskAF24zM2hpo; COMPASS=spreadsheet_forms%3DCjIACWuJVzailvltnDewjNjsoVIZb7xceiCmiCMf-b6hcn30OHqnlfrsyNJVUnvFNrEsDhCUtpWhBho0AAlriVdkhpdoaR9KpQ7Eag2VQAKWjb8_dWpFhslYXyPc_BdPNveY0wnescN1h0lY4bPWYA%3D%3D; NID=511%3DvD5dC6ceGIQvhNnUBpyhNJfl8GFupoWLTP6SDNovupZr2RrTo-rENaob1MLdam3vEOjHwev4fYroZzn9PO0RKWeCSJr7bVRxPK5O73-uZgTAO_ODiU05cAjMw2_PxCyj8TUXjOiUGOqzd4YxVYYvvM50dcA38RGHZ8MY-0CC-1A",
   CURLOPT_HTTPHEADER => [
     "Content-Type: application/x-www-form-urlencoded"
@@ -28,15 +28,9 @@ $err = curl_error($curl);
 
 curl_close($curl);
 
+
 if ($err) {
-  echo "cURL Error #:" . $err;
+    header('Location: /index.html?sacerr' + err,true, 302);
 } else {
-  echo $response;
+    header('Location: /index.html?sacses',true, 302);
 }
-
-// if ($err) {
-//     header('Location: /index.html?sacerr' + err,true, 302);
-// } else {
-//     header('Location: /index.html?sacses',true, 302);
-// }
-
